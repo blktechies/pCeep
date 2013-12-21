4b56404f8d2e3d2f213ab5ff39b8713c8956264c
Alvaro Muir <alvaromuir@gmail.com>
2013-12-20 22:11:09 -0500

    Pre-push, setup repo as python project

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
-pCeep
-=====
+## pullCommitpush
+### It's like speed dating, for technology start-up croud.
 
-Kinda like speed dating, for the tech community. Find and partner with the resources you need.
+----
+## What you need to know
+
+### app structure
+```
+.
+|- app
+     |-- models
+     |-- static
+     |-- templates
+     |-- views
+  |-- ui
+     |-- coffee
+     |-- sass
+  |-- tests
+```
+---
+
+### setup:
+```
+  $ npm install && bower install
+  $ pip install -r requirements.txt
+  $ python manage.py devserver
+  $ ./db_create.py
+  $ ./db_seed.py
+  $ python manage.py dev
+```
+Now the dev server is running, try to log in with a social account.
+Once logged in, back in terminal create some dummy messages to yourself:
+```
+$ ./seed_messages
+```
+
+
+Here's the deal:
+NPM sets up grunt and related packages. That includes COMPASS, which requires RUBY (*shakes fist).
+Bower sets up frontend stuff, like jquery, moment, socket-io, etc. You can add more as you need.
+Grunt compiles coffeescript and scss files to /app/static. 
+
+```
+$ grunt build 
+```
+will concat, minify and uglify files for production. For front-end work, run:
+```
+$ grunt watch
+
+```
+
+
+Alvaro Muir, @alvaromuir
---
+++ seed_data.py
@@ -0,0 +1,323 @@
+DESCRIPTIONS = [
+    'just another techie',
+    'web developer',
+    'software engineer',
+    'mobile developer',
+    'web designer',
+    'mobile designer',
+    'information architect',
+    'interaction designer',
+    'systems administrator',
+    'database administrator',
+    'network engineer',
+    'qualty assurance',
+    'marketer',
+    'founder',
+    'investor',
+    'student',
+]
+
+USERS = {
+    'beyonce': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '123456789t',
+                'username': 'beyonce',
+                'network_url': 'https://twitter.com/beyonce'
+            },
+        },
+        'account': {
+            'email': 'beyonce@pullcommitpush.com',
+            'display_name': 'Beyonce Carter',
+            'handle': 'beyonce',
+            'image_url': '/static/img/dev/avatars/beyonce.jpg',
+            'status': 'Who runs the world? Girls.'
+        }
+    },
+    'pbo': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '223456789t',
+                'username': 'barackobama',
+                'network_url': 'https://twitter.com/barackobama'
+            },
+            'linkedn': {
+                'network':'linkedin',
+                'network_id': '223456789l',
+                'username': 'barackobama',
+                'network_url': 'https://linkedin.com/in/barackobama'
+            }
+        },
+        'account': {
+            'email': 'obama@pullcommitpush.com',
+            'display_name': 'Barack Obama',
+            'handle': 'obama',
+            'image_url': '/static/img/dev/avatars/pbo.jpg',
+            'status': 'Change, you can believe in.'
+        }
+    },
+    'jennifer': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '323456789t',
+                'username': 'jlo',
+                'network_url': 'https://twitter.com/jlo'
+            }
+        },
+        'account': {
+            'email': 'jlo@pullcommitpush.com',
+            'display_name': 'Jennifer Lopez',
+            'handle': 'jennifer',
+            'image_url': '/static/img/dev/avatars/jennifer.jpg',
+            'status': 'Let\'s get loud.'
+        }
+    },
+    'justin': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '423456789t',
+                'username': 'jtimberlake',
+                'network_url': 'https://twitter.com/jtimberlake'
+            }
+        },
+        'account': {
+            'email': 'justin@pullcommitpush.com',
+            'display_name': 'Justin Timberlake',
+            'handle': 'justin',
+            'image_url': '/static/img/dev/avatars/justin.jpg',
+            'status': 'CEO, the NEW myspace.com'
+        }
+    },
+    'kanye': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '523456789t',
+                'username': 'kanyewest',
+                'network_url': 'https://twitter.com/kanyewest'
+            }
+        },
+        'account': {
+            'email': 'kanye@pullcommitpush.com',
+            'display_name': 'Kanye West',
+            'handle': 'yeezy',
+            'image_url': '/static/img/dev/avatars/kanye.jpg',
+            'status': 'I\'m the God of all things...'
+        }
+    },
+    'kevin': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '623456789t',
+                'username': 'KevinHart4real',
+                'network_url': 'https://twitter.com/KevinHart4real'
+            }
+        },
+        'account': {
+            'email': 'kevin@pullcommitpush.com',
+            'display_name': 'Kevin Hart',
+            'handle': 'kevin',
+            'image_url': '/static/img/dev/avatars/kevin.jpg',
+            'status': 'Say it witcha chest'
+        }
+    },
+    'lebron': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '723456789t',
+                'username': 'KingJames',
+                'network_url': 'https://twitter.com/KingJames'
+            }
+        },
+        'account': {
+            'email': 'lebron@pullcommitpush.com',
+            'display_name': 'LeBron James',
+            'handle': 'kingjames23',
+            'image_url': '/static/img/dev/avatars/lebron.jpg',
+            'status': 'Witness greatness'
+        }
+    },
+    'questlove': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '823456789t',
+                'username': 'questlove',
+                'network_url': 'https://twitter.com/questlove'
+            }
+        },
+        'account': {
+            'email': 'questlove@pullcommitpush.com',
+            'display_name': 'Ahmir Thompson',
+            'handle': 'questlove',
+            'image_url': '/static/img/dev/avatars/questlove.jpg',
+            'status': 'Little drumma boy'
+        }
+    },
+    'rihanna': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '823456789t',
+                'username': 'rihanna',
+                'network_url': 'https://twitter.com/rihanna'
+            }
+        },
+        'account': {
+            'email': 'rihanna@pullcommitpush.com',
+            'display_name': 'Robin Fenty',
+            'handle': 'rihanna',
+            'image_url': '/static/img/dev/avatars/rihanna.jpg',
+            'status': 'Bad gyal ri-ri'
+        }
+    },
+    'spike': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '823456789t',
+                'username': 'SpikeLee',
+                'network_url': 'https://twitter.com/SpikeLee'
+            }
+        },
+        'account': {
+            'email': 'spikelee@pullcommitpush.com',
+            'display_name': 'Spike Lee',
+            'handle': 'spikelee',
+            'image_url': '/static/img/dev/avatars/spike.jpg',
+            'status': 'Always doing the right thang'
+        }
+    },
+    'tyson': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '823456789t',
+                'username': 'MikeTyson',
+                'network_url': 'https://twitter.com/MikeTyson'
+            }
+        },
+        'account': {
+            'email': 'tyson@pullcommitpush.com',
+            'display_name': 'Mike Tyson',
+            'handle': 'ironmike',
+            'image_url': '/static/img/dev/avatars/tyson.jpg',
+            'status': 'Undesputed heavyweight champ of the world'
+        }
+    }
+}
+
+POSTS = [ 
+    {
+        'title': 'Python tutor needed',
+        'body': 'I am looking for someone who is able to help me with a python project. I am already halfway finished and need help completing this particular project. If you can help please email me and I will reply immediately'
+    },
+    {
+        'title': 'Business Development / Partnership',
+        'body': 'Looking for an individual that is social media savvy, can build a website and drive business development for an Equity Day Trading entity. Highly lucrative with residual income.'
+    },
+    {
+        'title': '3D Game Programmer Needed',
+        'body': 'I\'m leading a small team and we\'re in need of an experienced 3D game programmer for an open-world sandbox MMO Kickstarter project expected to launch in mid-February of 2014. I am looking for an individual(s) to join an already existing team!'
+    },
+    {
+        'title': 'Freelance designer seeks front End Web Developer',
+        'body': 'Help me, help you. Looking for someone to assist with content development. CMS data entry (text, html, xml, javascript) in support of new website launch.'
+    },
+    {
+        'title': 'I Want To Start a Computer/Cell Phone Repair Business',
+        'body': 'Figured I would try my luck here.I want people that have experience repairing cell phones, and or computers. I\'m looking to form a company.'
+    },
+    {
+        'title': 'Mobile developer seeks mobile designer',
+        'body': 'Me - design appreciating, deadline respecting left-brainer. You Sketch using, web-safe font incorporating pixel pusher. Let\'s connect and make beautiful mobile apps together.'
+    }
+]
+
+COMMENTS = [
+    "That's a great idea, how can I get down?",
+    "Has this been done before?",
+    "First time for everything",
+    "I heard that hot ish",
+    "I rely on Bed-Stuy to hold me down if I die",
+    "Never trust a big butt and a smile.",
+    "My mentality is money-orientated.",
+    "You think so?",
+    "I can hit you with real millionare shit",
+    "Comment much?",
+    "Brooklyn, we go hard",
+    "PHP FTW",
+    "PHP? FOH",
+    "Android or die!",
+    "I googled it. Didn't you?",
+    "Follow me on twitter",
+    "How about some hardcore? Yea we like it raw!",
+    "When I steps up in the party you know I steps correct",
+    "Bum-stiggety-bum-stiggety-bum",
+    "May, I, kick a lil sumptin' fo da G'z who",
+    "Well here's a funky little introduction of how nice I am",
+    "Out and bad so badly bad.",
+    "A-B-C. Easy as 1-2-3",
+    "Watch my github repo"
+]
+
+INTERESTS = [
+    "ASP",
+    "ASP.NET",
+    'big data',
+    "Biz dev",
+    "C",
+    "C++",
+    "C#",
+    "Cassandra",
+    "CouchDB",
+    "CSS",
+    "DBA",
+    'data mining',
+    "Django",
+    "Erlang",
+    "E-commerce",
+    'entreprenuial',
+    "founding",
+    "Groovy",
+    "Haskell",
+    "Hadoop",
+    "HTML5",
+    "illustrator",
+    "investing",
+    "iOS",
+    "Java",
+    "JavaScript",
+    "Joomla",
+    "Js",
+    "Linux",
+    "marketing",
+    "MongoDB",
+    "MySQL",
+    "NoSQL",
+    "RUBY",
+    "Ruby on Rails",
+    "NodeJS",
+    "Object-C",
+    "Oracle",
+    "Perl",
+    "Photoshop",
+    "PHP",
+    "PostgreSQL",
+    "Python",
+    "Sketch",
+    "systems administration",
+    "SQL",
+    "SQL Server",
+    "Unix",
+    'venture capital',
+    "Windows",
+    "Wordpress",
+    "Zend PHP"
+]
